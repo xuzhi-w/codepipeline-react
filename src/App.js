aebb4544175b0aa06afe29b072b30d88e3201755
xuzhi <compro.xwei@gmail.com>
2025-06-15 21:02:23 -0500

    update h1

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ function App() {
     <div className="App">
       <h1>Hello World, What a nice day I have! </h1>
       <h1>You must be a genius to have come so far!</h1>
-  
+  <h1>Hello World, What a nice day I have! </h1>
+      <h1>You must be a genius to have come so far!</h1>
     </div>
   );
 }
