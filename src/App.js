37800e4a1b5e2ef1e03e527184dd9e5790c8515b
xuzhi <compro.xwei@gmail.com>
2025-06-15 21:36:17 -0500

    hello

--- src/App.js
+++ src/App.js
@@ -7,12 +7,7 @@ function App() {
       <h1>Hello World, What a nice day I have! </h1>
       <h1>You must be a genius to have come so far!</h1>
       <h1>my-webapp-pipeline</h1>
-      <h1>Hello World, What a nice day I have! </h1>
-      <h1>You must be a genius to have come so far!</h1>
-      <h1>my-webapp-pipeline</h1>
-      <h1>Hello World, What a nice day I have! </h1>
-      <h1>You must be a genius to have come so far!</h1>
-      <h1>my-webapp-pipeline</h1>
+     
     </div>
   );
 }
